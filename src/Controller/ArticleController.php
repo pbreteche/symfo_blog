782915a18e597bd1bc4ad819c4d66e37d19b85e3
Pierre Bretéché <pbreteche@dawan.fr>
2019-10-21 17:04:10 +0200

    liste des articles

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index(ArticleRepository $repository)
+    {
+        $articles = $repository->findAll();
+
+        return $this->render('article/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+}
