2e3356158f23391976d4ba1e6ae3abb613a209ef
Pierre Bretéché <pbreteche@dawan.fr>
2019-10-22 16:22:18 +0200

    page création article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Author;
 use App\Repository\ArticleRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
@@ -28,6 +32,45 @@ class ArticleController extends AbstractController
     {
         return $this->render('article/show.html.twig', [
             'article' => $article,
+        ]);
+    }
+
+    /**
+     * @Route("/new", methods={"GET", "POST"})
+     */
+    public function new(Request $request)
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', null, [
+                'label' => 'Titre',
+            ])
+            ->add('content')
+            ->add('publishedAt', DateTimeType::class, [
+                'widget' => 'single_text',
+                'required' => false,
+            ])
+            ->add('writtenBy', EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => 'name'
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('app_article_show', [
+                'id' => $article->getId(),
+            ]);
+        }
+
+        return $this->render('article/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
