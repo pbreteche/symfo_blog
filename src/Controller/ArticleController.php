e2de1dda33c00858b6e3fa00f688fecd2c821925
Pierre Bretéché <pbreteche@dawan.fr>
2019-10-22 09:51:08 +0200

    page de détail d'article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -19,4 +19,20 @@ class ArticleController extends AbstractController
             'articles' => $articles,
         ]);
     }
+
+    /**
+     * @Route("/{id}", requirements={"id":"\d+"}, methods="GET")
+     */
+    public function show(string $id, ArticleRepository $repository)
+    {
+        $article = $repository->find($id);
+
+        if(!$article) {
+            throw $this->createNotFoundException('Article inexistant');
+        }
+
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
 }
