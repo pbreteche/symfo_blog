cabcbd0c7087df0ab09deb2523e53c8d38951ce2
Pierre Bretéché <pbreteche@dawan.fr>
2019-10-25 14:51:48 +0200

    test plusieurs jeux de données

--- tests/Service/SearchQueryParserTest.php
+++ tests/Service/SearchQueryParserTest.php
@@ -7,11 +7,25 @@ use PHPUnit\Framework\TestCase;
 
 class SearchQueryParserTest extends TestCase
 {
-    public function testParse()
+
+    /**
+     * @dataProvider provideTestParse
+     */
+    public function testParse($input, $expected)
     {
         $parser = new SearchQueryParser();
 
-        $term = $parser->parse('rugby poulet symfony');
-        $this->assertEquals('rugby', $term);
+        $term = $parser->parse($input);
+        $this->assertEquals($expected, $term);
+    }
+
+    public function provideTestParse()
+    {
+        return [
+            ['rugby poulet symfony', 'rugby'],
+            ['KeBaB falafel aéèîù', 'kebab'],
+            ['   PHP MySQL Linux', 'php'],
+            ['', ''],
+        ];
     }
 }
